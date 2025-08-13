3010a35adeae18ffb8800e9df17889a7c20025f3
Atombombskatt <bjorn.samuelson96rgmail.com>
2025-08-13 16:45:37 +0200

    added recipes page and recipedetailpage

---
+++ src/data/RecipesData.ts
@@ -0,0 +1,4 @@
+export const recipes: {id: string, title: string, description: string}[] = [
+    {id: "1", title: "Banan & Choklad Kaka", description: "Proteinfylld kaka med smak av banan och choklad" },
+    {id: "2", title: "Panerad Kyckling", description: "Panerade Kycklingbröst med grönsaker och riven parmesan" },
+    ];
